e22d8661720d86e08b47a320f832b186cb7a18e3
hamzamouhamadi <ton email>
2024-07-26 10:36:51 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { COURSES } from 'src/db-data';
+import { Course } from './model/course';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app2';
+  title = 'App-';
+  courseMe = COURSES[0];
+  courseMe2 = COURSES[1];
+  courseMe3 = COURSES[2];
+
+  onCourseSelected(course:Course){
+    console.log
+    ("APP-comp...",course)
+  }
 }
